633c22d5909ab91f9e0f4b14371b9bd4a20f75a9
Cesur APAYDIN <apaydin541@gmail.com>
2024-07-09 01:05:38 +0300

    Update ZenTruckFoundry

--- src/Factory/MediaFactory.php
+++ src/Factory/MediaFactory.php
@@ -4,31 +4,12 @@ namespace Cesurapp\MediaBundle\Factory;
 
 use Cesurapp\MediaBundle\Entity\Media;
 use Cesurapp\MediaBundle\Manager\MediaManager;
-use Cesurapp\MediaBundle\Repository\MediaRepository;
-use Zenstruck\Foundry\ModelFactory;
-use Zenstruck\Foundry\Proxy;
-use Zenstruck\Foundry\RepositoryProxy;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
 
 /**
- * @extends ModelFactory<Media>
- *
- * @method        Media|Proxy                     create(array|callable $attributes = [])
- * @method static Media|Proxy                     createOne(array $attributes = [])
- * @method static Media|Proxy                     find(object|array|mixed $criteria)
- * @method static Media|Proxy                     findOrCreate(array $attributes)
- * @method static Media|Proxy                     first(string $sortedField = 'id')
- * @method static Media|Proxy                     last(string $sortedField = 'id')
- * @method static Media|Proxy                     random(array $attributes = [])
- * @method static Media|Proxy                     randomOrCreate(array $attributes = [])
- * @method static MediaRepository|RepositoryProxy repository()
- * @method static Media[]|Proxy[]                 all()
- * @method static Media[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
- * @method static Media[]|Proxy[]                 createSequence(iterable|callable $sequence)
- * @method static Media[]|Proxy[]                 findBy(array $attributes)
- * @method static Media[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
- * @method static Media[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @extends PersistentProxyObjectFactory<Media>
  */
-final class MediaFactory extends ModelFactory
+final class MediaFactory extends PersistentProxyObjectFactory
 {
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
@@ -40,12 +21,17 @@ final class MediaFactory extends ModelFactory
         parent::__construct();
     }
 
+    public static function class(): string
+    {
+        return Media::class;
+    }
+
     /**
      * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
      *
      * @todo add your default values here
      */
-    protected function getDefaults(): array
+    protected function defaults(): array|callable
     {
         $content = file_get_contents(__DIR__.'/../../tests/resources/image.png');
         $media = $this->mediaManager
@@ -61,10 +47,5 @@ final class MediaFactory extends ModelFactory
             'size' => $media->getSize(),
             'storage' => $media->getStorage(),
         ];
-    }
-
-    protected static function getClass(): string
-    {
-        return Media::class;
     }
 }
