711b6744f5a8985b72f20fda6e5b41d4cac93466
Cesur APAYDIN <apaydin541@gmail.com>
2026-04-16 21:06:44 +0700

    Fix

--- src/EventListener/MediaRemovedListener.php
+++ src/EventListener/MediaRemovedListener.php
@@ -4,19 +4,19 @@ namespace Cesurapp\MediaBundle\EventListener;
 
 use Cesurapp\StorageBundle\Storage\Storage;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
-use Doctrine\ORM\Event\PreRemoveEventArgs;
+use Doctrine\ORM\Event\PostRemoveEventArgs;
 use Doctrine\ORM\Events;
 use Cesurapp\MediaBundle\Entity\Media;
 use Psr\Log\LoggerInterface;
 
-#[AsEntityListener(event: Events::preRemove, method: 'preRemove', entity: Media::class)]
+#[AsEntityListener(event: Events::postRemove, method: 'postRemove', entity: Media::class)]
 readonly class MediaRemovedListener
 {
     public function __construct(private Storage $storage, private LoggerInterface $logger)
     {
     }
 
-    public function preRemove(Media $media, PreRemoveEventArgs $event): void
+    public function postRemove(Media $media, PostRemoveEventArgs $event): void
     {
         if (function_exists('go')) {
             go(function () use ($media) {
