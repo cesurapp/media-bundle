bb99c82e3ffd5983af983f32720c52b0e6444883
Cesur APAYDIN <apaydin541@gmail.com>
2026-04-16 22:00:47 +0700

    Fİx

--- src/EventListener/MediaRemovedListener.php
+++ src/EventListener/MediaRemovedListener.php
@@ -4,29 +4,52 @@ namespace Cesurapp\MediaBundle\EventListener;
 
 use Cesurapp\StorageBundle\Storage\Storage;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Event\PreRemoveEventArgs;
 use Doctrine\ORM\Event\PostRemoveEventArgs;
 use Doctrine\ORM\Events;
 use Cesurapp\MediaBundle\Entity\Media;
 use Psr\Log\LoggerInterface;
 
+#[AsEntityListener(event: Events::preRemove, method: 'preRemove', entity: Media::class)]
 #[AsEntityListener(event: Events::postRemove, method: 'postRemove', entity: Media::class)]
-readonly class MediaRemovedListener
+class MediaRemovedListener
 {
-    public function __construct(private Storage $storage, private LoggerInterface $logger)
+    private \SplObjectStorage $pendingRemovals;
+
+    public function __construct(private readonly Storage $storage, private readonly LoggerInterface $logger)
     {
+        $this->pendingRemovals = new \SplObjectStorage();
+    }
+
+    public function preRemove(Media $media, PreRemoveEventArgs $event): void
+    {
+        $this->pendingRemovals->attach($media, [
+            'path'    => $media->getPath(),
+            'storage' => $media->getStorage(),
+        ]);
     }
 
     public function postRemove(Media $media, PostRemoveEventArgs $event): void
     {
+        if (!$this->pendingRemovals->contains($media)) {
+            return;
+        }
+
+        $data  = $this->pendingRemovals[$media];
+        $this->pendingRemovals->detach($media);
+
+        $path    = $data['path'];
+        $store   = $data['storage'];
+
         if (function_exists('go')) {
-            go(function () use ($media) {
-                if (!$this->storage->device($media->getStorage())->delete($media->getPath())) {
-                    $this->logger->error('Media File Remove Failed: '.$media->getStorage().'::'.$media->getPath());
+            go(function () use ($path, $store) {
+                if (!$this->storage->device($store)->delete($path)) {
+                    $this->logger->error('Media File Remove Failed: '.$store.'::'.$path);
                 }
             });
         } else {
-            if (!$this->storage->device($media->getStorage())->delete($media->getPath())) {
-                $this->logger->error('Media File Remove Failed: '.$media->getStorage().'::'.$media->getPath());
+            if (!$this->storage->device($store)->delete($path)) {
+                $this->logger->error('Media File Remove Failed: '.$store.'::'.$path);
             }
         }
     }
